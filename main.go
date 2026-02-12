a3e39fde35067e9c02914f493a826581d5ebbcbd
lu2ky <jaaa736504@gmail.com>
2026-02-12 17:39:33 -0500

    Information: the documentation for all things are added

--- main.go
+++ main.go
@@ -12,16 +12,17 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
+	err := godotenv.Load() // Load enviorement variables
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal(".env file (error corrupted/not found)")
 	}
-	cfg := mysql.NewConfig()
+	cfg := mysql.NewConfig()          //Create the cfg for MySQL
 	cfg.User = os.Getenv("DB_USER")   //User
 	cfg.Passwd = os.Getenv("DB_PASS") //Pass
 
-	router := gin.Default()
-	router.GET("ruta", method)
-	router.Run("localhost:3912")
+	router := gin.Default()    //Create the default router for POST/GET methods
+	router.GET("ruta", method) /* Use the / for subdirectorys in the localhost:3912
+	and references the method 						*/
+	router.Run("localhost:3912") // The port number for expone the API
 }
-func method(c *gin.Context) {}
+func method(c *gin.Context) {} // c *gin.Context essential for method in GET/POST actions
