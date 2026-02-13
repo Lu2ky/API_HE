57a1296b65b9884667906521859af68dcfc4d54a
lu2ky <jaaa736504@gmail.com>
2026-02-13 18:12:53 -0500

    Feat: Se añadio una consulta escencial para el desarrollo del aplicativo

--- main.go
+++ main.go
@@ -21,6 +21,18 @@ type User struct {
 	Name     string `json:"T_nombre"`
 	Programa string `json:"T_programa"`
 }
+type Schedule struct {
+	Nrc        string          `json:"nrc_curso"`
+	Nombre     string          `json:"nombre_curso"`
+	Etiqueta   string          `json:"etiqueta_curso"`
+	Dia        int             `json:"dia_curso"`
+	HoraIni    string          `json:"hora_inicio_curso"`
+	HoraFin    string          `json:"hora_fin_curso"`
+	Salon      string          `json:"salon_curso"`
+	Creditos   sql.NullFloat64 `json:"creditos_curso"`
+	Mcalificar string          `json:"mcalificar_curso"`
+	Campus     string          `json:"campus_curso"`
+}
 
 func main() {
 	err := godotenv.Load() // Load enviorement variables
@@ -39,10 +51,9 @@ func main() {
 		log.Fatal("Error connecting to database:", err2)
 	}
 	defer db.Close()
-	defer db.Close()
 	router := gin.Default()                     //Create the default router for POST/GET methods
-	router.GET("/GetUserById/:id", getUserById) /* Use the / for subdirectorys in the localhost:3912
-	and references the method 						*/
+	router.GET("/GetUserById/:id", getUserById) /* Use the / for subdirectorys in the localhost:3912 and references the method */
+	router.GET("/GetScheduleByUserId/:id", getScheduleByUserId)
 	router.Run("localhost:3912") // The port number for expone the API
 }
 func method(c *gin.Context) {}
@@ -56,6 +67,7 @@ func getUserById(c *gin.Context) {
 	/* Extract the id param for the query	*/
 	var UserSaved User
 	/* Put the param in the query remplace '?' for the id */
+
 	err := db.QueryRow("SELECT T_codUsuario, T_nombre, T_programa FROM Usuarios WHERE T_codUsuario = ?", id).Scan(&UserSaved.Id, &UserSaved.Name, &UserSaved.Programa)
 	/* test the insert the query in the database */
 	if err != nil {
@@ -68,4 +80,39 @@ func getUserById(c *gin.Context) {
 		return
 	}
 	c.JSON(200, UserSaved) /* 200 = post the json in http */
+}
+func getScheduleByUserId(c *gin.Context) {
+	id := c.Param("id")
+	var rows *sql.Rows
+	var err error
+	var r string
+	var err2 error
+	err2 = db.QueryRow("SELECT N_idUsuario FROM Usuarios WHERE T_codUsuario= ? ", id).Scan(&r)
+	if err2 != nil {
+		log.Printf("Database error: %v", err)
+		c.JSON(500, gin.H{"error": "Internal server error"}) //ERROR 500
+		return
+	}
+	rows, err = db.Query("SELECT c.T_nrcCurso AS 'NRC', c.T_nombre AS 'Curso',  e.T_nombre AS 'Etiqueta', d.N_dia AS 'Dia', d.TM_horaInicio AS 'Hora de inicio', d.TM_horaFin AS 'Hora de fin', d.T_salon AS 'Salón', c.N_creditos AS 'Créditos', c.T_modoCalificar AS 'Modo de calificar', c.T_campus AS 'Campus' FROM Cursos c, Etiqueta e, horario h, dias_clase d, Materia_has_dias_clase m WHERE c.N_idCurso=h.N_idCurso AND d.N_idDiasCase=m.N_idDiasClase AND c.N_idCurso=m.N_idCurso AND c.N_idEtiqueta= e.N_idEtiqueta AND h.N_idUsuario= ? ", r)
+	if err != nil {
+		log.Printf("Database error: %v", err)
+		c.JSON(500, gin.H{"error": "Internal server error"}) //ERROR 500
+		return
+	}
+	defer rows.Close()
+
+	var schedules []Schedule
+	for rows.Next() {
+		var schedule Schedule
+		err := rows.Scan(&schedule.Nrc, &schedule.Nombre, &schedule.Etiqueta, &schedule.Dia, &schedule.HoraIni, &schedule.HoraFin, &schedule.Salon, &schedule.Creditos, &schedule.Mcalificar, &schedule.Campus)
+		if err != nil {
+			log.Printf("Scan error: %v", err)
+			c.JSON(500, gin.H{"Error": "Error en procesamiento de datos"})
+			return
+		}
+		schedules = append(schedules, schedule)
+
+	}
+	c.JSON(200, schedules)
+
 }
